fedd9a757d104fd716c49d60a12a439492a9302c
anjanayraina <anjanayraina326@gmail.com>
2023-11-22 10:19:47 +0530

    Foundry bug resolved

--- src/Contracts/PerpetualVault.sol
+++ src/Contracts/PerpetualVault.sol
@@ -1,19 +1,19 @@
-pragma solidity 0.8.20;
+pragma solidity 0.8.21;
 
 // testnet oracle : https://sepolia.etherscan.io/address/0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD
 // wBTC/USDC pool
 // how is the value of a share of the pool set ??
 
-// import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import IERC20 from "../Interfaces/IERC20.sol";
+
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+
 contract PerpetualVault  is ERC4626{
     // using Counters for Counters.Counter;
     // Counters.Counter private currentPositionID;
     uint8 public constant MAX_LEVERAGE = 20;
     uint8 public gasStipend;
     IERC20 public wBTCToken;
-    IERC20 public USDCtoken;
+    IERC20 public USDCToken;
     struct Position {
         address creator;
         uint collateral;
@@ -22,9 +22,9 @@ contract PerpetualVault  is ERC4626{
         uint256 positionID;
     }
     constructor(IERC20 LPTokenAddress ,IERC20 BTCTokenAddress , string memory name , string memory symbol) ERC4626(LPTokenAddress) ERC20(name , symbol){
-        BTCToken = BTCTokenAddress;
+        wBTCToken = BTCTokenAddress;
         USDCToken= LPTokenAddress;
-        // currentPositionID.increment();
+        
     }
 
     function getBTCAddress() public view  returns(IERC20){
--- src/Interfaces/IERC20.sol
+++ src/Interfaces/IERC20.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.7;
+pragma solidity ^0.8.20;
 interface IERC20 {
     function transferFrom(address from , address to , uint amount) external returns (bool);
     function approve() external returns(bool);
