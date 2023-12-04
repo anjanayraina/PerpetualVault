1a7725c05a4c815a55e1628ed438c22cdc9c75ed
anjanayraina <anjanayraina326@gmail.com>
2023-12-04 19:38:12 +0530

    Price check test added

--- src/PriceFeed/ChainLinkPriceFeed.sol
+++ src/PriceFeed/ChainLinkPriceFeed.sol
@@ -8,18 +8,21 @@ contract ChainLinkPriceFeed is Ownable{
         AggregatorV3Interface primaryPriceFeed;
         AggregatorV3Interface secondaryPriceFeed;
         int256 lastGoodPrice;
+        uint8 decimals;
     }
 
     uint256 constant TIMEOUT = 1000; 
+    uint256 constant MAX_ALLOWED_DEVIATION = 10;
     mapping(string=>PriceFeeds) tokenNameToPriceFeed;
     constructor(address owner) Ownable(owner){
 
     }
-    function addToken(string calldata tokenName , address primaryFeedAddress ,address secondaryFeedAddress , int256 intialPrice) external onlyOwner{
+    function addToken(string calldata tokenName , address primaryFeedAddress ,address secondaryFeedAddress , int256 intialPrice , uint8  decimals) external onlyOwner{
         PriceFeeds storage feed = tokenNameToPriceFeed[tokenName];
         feed.primaryPriceFeed = AggregatorV3Interface(primaryFeedAddress);
         feed.secondaryPriceFeed = AggregatorV3Interface(secondaryFeedAddress); 
         feed.lastGoodPrice = intialPrice;
+        feed.decimals = decimals;
     }
 
     // function changePrimaryFeed(string calldata tokenName , address primaryFeedAddress) external onlyOwner{
@@ -34,13 +37,13 @@ contract ChainLinkPriceFeed is Ownable{
     function getPrice(string calldata tokenName ) external returns(int256 )  {
         PriceFeeds memory feed = tokenNameToPriceFeed[tokenName];
         (uint80 roundID , int256 price , uint256 startedAt , uint256 updatedAt , uint80 answeredInRound) = feed.primaryPriceFeed.latestRoundData();
-        if(roundID != 0 && price >=0 && updatedAt <= block.timestamp && (block.timestamp - updatedAt) <TIMEOUT ){
+        if(roundID != 0 && price >=0 && updatedAt <= block.timestamp && (block.timestamp - updatedAt) <TIMEOUT && _absoluteValue(price - feed.lastGoodPrice) < MAX_ALLOWED_DEVIATION ){
             feed.lastGoodPrice = price;
             return price;
         }
 
        ( roundID ,  price ,  startedAt ,  updatedAt ,  answeredInRound) = feed.secondaryPriceFeed.latestRoundData();
-       if(roundID != 0 && price >=0 && updatedAt <= block.timestamp && (block.timestamp - updatedAt) <TIMEOUT ){
+       if(roundID != 0 && price >=0 && updatedAt <= block.timestamp && (block.timestamp - updatedAt) <TIMEOUT  && _absoluteValue(price - feed.lastGoodPrice) < MAX_ALLOWED_DEVIATION){
             feed.lastGoodPrice = price;
             return price;
         }
@@ -49,7 +52,9 @@ contract ChainLinkPriceFeed is Ownable{
 
     }
 
-    
+    function _absoluteValue(int256 value) internal pure  returns (uint256) {
+        return uint256(value >= 0 ? value : -value);
+    }
 
 
 }
---
+++ test/ChainLinkPriceFeed.t.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ChainLinkPriceFeed} from "../src/PriceFeed/ChainLinkPriceFeed.sol";
+import {USDC} from "../src/Tokens/USDCToken.sol";
+import {WBTCToken} from "../src/Tokens/WBTCToken.sol";
+import {AggregatorV3Contract} from "../src/Oracle/AggregatorV3Contract.sol";
+
+contract ChainLinkPriceFeedTest is Test {
+    USDC usdcToken;
+    AggregatorV3Contract usdcOracle1;
+    AggregatorV3Contract usdcOracle2;
+    ChainLinkPriceFeed feed ;
+
+     function setUp() public {
+        usdcToken = new USDC(address(1));
+        usdcOracle1 = new AggregatorV3Contract(address(1) , usdcToken.decimals() , 1 , "USDC Oracle");
+        usdcOracle2 = new AggregatorV3Contract(address(1) , usdcToken.decimals() , 1, "USDC Oracle");
+        feed = new ChainLinkPriceFeed(address(1));
+        feed.addToken("USDC" , address(usdcOracle1) ,address(usdcOracle2) , 1 , usdcToken.decimals());
+
+
+    }
+
+    function test_PriceCheck() public {
+
+    }
+
+}
