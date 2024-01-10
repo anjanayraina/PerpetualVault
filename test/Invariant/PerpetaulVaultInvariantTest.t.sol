3fb0930b57bc51f510b9c2a92bf14339514ab066
anjanayraina <anjanayraina326@gmail.com>
2024-01-10 10:48:16 +0530

    PerpetualVaultInvariantTest::openPosition added

--- test/Invariant/PerpetaulVaultInvariantTest.t.sol
+++ test/Invariant/PerpetaulVaultInvariantTest.t.sol
@@ -50,7 +50,6 @@ contract PerpetualVaultInvariantTest is Test {
         vm.assume(size > 100);
         vm.assume(size < type(uint128).max);
         vm.assume(size > collateral);
-        vm.assume(size/100 < type(uint256).max);
         vm.assume((collateral)/(10 ** (usdcToken.decimals()+1)) < type(uint256).max);
         vm.assume(size / collateral < 21);
         vm.startPrank(address(1));
